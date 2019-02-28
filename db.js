57bd5fed734321cb16a6f15fc7e7ef23af88400f
camendoza94 <camendoza94@gmail.com>
2019-02-28 18:07:23 -0500

    Changed db

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 "use strict";
 const MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
-const MONGO_URI = process.env.MONGOLAB_URI || 'mongodb://user:user@localhost:27017/locs';
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://user:user@localhost:27017/locs';
 mongoose.connect(MONGO_URI, {
   useMongoClient: true,
 });
