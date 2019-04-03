e37d1996ce2ad251565589b00460b2f57f1b3d92
Camilo Mendoza <camendoza94@gmail.com>
2019-04-03 13:08:38 -0500

    Added support for puts

--- models/commitDate.js
+++ models/commitDate.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const commitDateSchema = new Schema({
     name: {type: String, required: true},
     dates: {type: [], required: true}
-});
+}, { usePushEach: true });
 
 const CommitDate = mongoose.model('CommitDate', commitDateSchema);
 
