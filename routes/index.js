c8e17ad4144a613484ea1f0ff2dd9d61b54534fe
Camilo Mendoza <camendoza94@gmail.com>
2019-09-24 12:50:58 -0500

    Better handling of history

--- routes/index.js
+++ routes/index.js
@@ -7,58 +7,58 @@ const History = require('../models/history');
 const File = require('../models/fileHistory');
 const Architecture = require('../models/architecture');
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-    app.get('/locs', function(req, res) {
-        Project.find(function(err, projects) {
+    app.get('/locs', function (req, res) {
+        Project.find(function (err, projects) {
             if (err)
                 return res.send(err);
             res.json(projects);
         });
     });
 
-    app.get('/issues', function(req, res) {
-        IssueList.find(function(err, issues) {
+    app.get('/issues', function (req, res) {
+        IssueList.find(function (err, issues) {
             if (err)
                 return res.send(err);
             res.json(issues);
         });
     });
 
-    app.get('/keys', function(req, res) {
-        Key.find(function(err, keys) {
+    app.get('/keys', function (req, res) {
+        Key.find(function (err, keys) {
             if (err)
                 return res.send(err);
             res.json(keys);
         });
     });
 
-    app.get('/dates', function(req, res) {
-        CommitDate.find(function(err, dates) {
+    app.get('/dates', function (req, res) {
+        CommitDate.find(function (err, dates) {
             if (err)
                 return res.send(err);
             res.json(dates);
         });
     });
 
-    app.get('/history', function(req, res) {
-        History.find(function(err, projects) {
+    app.get('/history', function (req, res) {
+        History.find(function (err, projects) {
             if (err)
                 return res.send(err);
             res.json(projects);
         });
     });
 
-    app.get('/files', function(req, res) {
-        File.find(function(err, projects) {
+    app.get('/files', function (req, res) {
+        File.find(function (err, projects) {
             if (err)
                 return res.send(err);
             res.json(projects);
         });
     });
 
-    app.get('/architecture', function(req, res) {
-        Architecture.find(function(err, projects) {
+    app.get('/architecture', function (req, res) {
+        Architecture.find(function (err, projects) {
             if (err)
                 return res.send(err);
             res.json(projects);
@@ -66,25 +66,25 @@ module.exports = function(app) {
     });
 
 
-    app.post('/issues', function(req, res) {
-        IssueList.remove({}, (err)=> {
-            if(err)
+    app.post('/issues', function (req, res) {
+        IssueList.remove({}, (err) => {
+            if (err)
                 return res.send(err);
             IssueList.collection.insert(req.body, (err, issues) => {
-                if(err)
+                if (err)
                     return res.send(err);
                 res.json(issues);
             });
         });
     });
 
-    app.post('/locs', function(req, res) {
+    app.post('/locs', function (req, res) {
         let project = new Project();
 
         project.name = req.body.name;
         project.locs = req.body.locs;
 
-        project.save(function(err) {
+        project.save(function (err) {
             if (err)
                 return res.send(err);
 
@@ -92,13 +92,13 @@ module.exports = function(app) {
         });
     });
 
-    app.post('/history', function(req, res) {
+    app.post('/history', function (req, res) {
         let project = new History();
 
         project.name = req.body.name;
         project.data = req.body.data;
 
-        project.save(function(err) {
+        project.save(function (err) {
             if (err)
                 return res.send(err);
 
@@ -106,13 +106,13 @@ module.exports = function(app) {
         });
     });
 
-    app.post('/files', function(req, res) {
+    app.post('/files', function (req, res) {
         let project = new File();
 
         project.name = req.body.name;
         project.data = req.body.data;
 
-        project.save(function(err) {
+        project.save(function (err) {
             if (err)
                 return res.send(err);
 
@@ -121,13 +121,13 @@ module.exports = function(app) {
     });
 
 
-    app.post('/dates', function(req, res) {
+    app.post('/dates', function (req, res) {
         let project = new CommitDate();
 
         project.name = req.body.name;
         project.dates = req.body.dates;
 
-        project.save(function(err) {
+        project.save(function (err) {
             if (err)
                 return res.send(err);
 
@@ -136,13 +136,13 @@ module.exports = function(app) {
     });
 
 
-    app.post('/keys', function(req, res) {
+    app.post('/keys', function (req, res) {
         let key = new Key();
 
         key.name = req.body.name;
         key.key = req.body.key;
 
-        key.save(function(err) {
+        key.save(function (err) {
             if (err)
                 return res.send(err);
 
@@ -150,49 +150,49 @@ module.exports = function(app) {
         });
     });
 
-    app.put('/locs/:name', function(req, res) {
-        Project.findOne({ name: req.params.name }, function(err, project) {
+    app.put('/locs/:name', function (req, res) {
+        Project.findOne({name: req.params.name}, function (err, project) {
             if (err) {
-                 return res.send(err);
+                return res.send(err);
             }
 
-            if(!project) {
-            	let p = new Project();
+            if (!project) {
+                let p = new Project();
                 p.name = req.params.name;
                 p.locs = [req.body.locs];
-                p.save(function(err) {
+                p.save(function (err) {
                     if (err)
                         return res.send(err);
 
                     return res.json(p);
                 });
             } else {
-	            project.name = req.body.name || project.name;
-	            if(req.body.locs)
-	                project.locs.push(req.body.locs);
+                project.name = req.body.name || project.name;
+                if (req.body.locs)
+                    project.locs.push(req.body.locs);
 
-	            project.save(function(err) {
-	                if (err)
-	                    return res.send(err);
+                project.save(function (err) {
+                    if (err)
+                        return res.send(err);
 
-	                res.json(project);
-	            });
-       		}
+                    res.json(project);
+                });
+            }
         });
     });
 
 
-    app.put('/dates/:name', function(req, res) {
-        CommitDate.findOne({ name: req.params.name }, function(err, project) {
+    app.put('/dates/:name', function (req, res) {
+        CommitDate.findOne({name: req.params.name}, function (err, project) {
             if (err) {
                 return res.send(err);
             }
 
-            if(!project) {
+            if (!project) {
                 let c = new CommitDate();
                 c.name = req.params.name;
                 c.dates = [req.body.dates];
-                c.save(function(err) {
+                c.save(function (err) {
                     if (err)
                         return res.send(err);
 
@@ -200,10 +200,10 @@ module.exports = function(app) {
                 });
             } else {
                 project.name = req.body.name || project.name;
-                if(req.body.dates)
+                if (req.body.dates)
                     project.dates.push(req.body.dates);
 
-                project.save(function(err) {
+                project.save(function (err) {
                     if (err)
                         return res.send(err);
 
@@ -213,17 +213,17 @@ module.exports = function(app) {
         });
     });
 
-    app.put('/history/:name', function(req, res) {
-        History.findOne({ name: req.params.name }, function(err, project) {
+    app.put('/history/:name', function (req, res) {
+        History.findOne({name: req.params.name}, function (err, project) {
             if (err) {
                 return res.send(err);
             }
 
-            if(!project) {
+            if (!project) {
                 let p = new History();
                 p.name = req.params.name;
                 p.data = [req.body.data];
-                p.save(function(err) {
+                p.save(function (err) {
                     if (err)
                         return res.send(err);
 
@@ -231,9 +231,12 @@ module.exports = function(app) {
                 });
             } else {
                 project.name = req.body.name || project.name;
-                if(req.body.data && !project.data.find(d => d.commitId === req.body.data.commitId))
+                const oldDataIndex = project.data.findIndex(d => d.commitId === req.body.data.commitId);
+                if (req.body.data && oldData)
+                    project.data[oldDataIndex] = req.body.data;
+                if (req.body.data && !oldData)
                     project.data.push(req.body.data);
-                project.save(function(err) {
+                project.save(function (err) {
                     if (err)
                         return res.send(err);
                     res.json(project);
@@ -242,17 +245,17 @@ module.exports = function(app) {
         });
     });
 
-    app.put('/files/:name', function(req, res) {
-        File.findOne({ name: req.params.name }, function(err, project) {
+    app.put('/files/:name', function (req, res) {
+        File.findOne({name: req.params.name}, function (err, project) {
             if (err) {
                 return res.send(err);
             }
 
-            if(!project) {
+            if (!project) {
                 let p = new File();
                 p.name = req.params.name;
                 p.data = [req.body.data];
-                p.save(function(err) {
+                p.save(function (err) {
                     if (err)
                         return res.send(err);
 
@@ -260,9 +263,12 @@ module.exports = function(app) {
                 });
             } else {
                 project.name = req.body.name || project.name;
-                if(req.body.data)
+                const oldDataIndex = project.data.findIndex(d => d.commitId === req.body.data.commitId);
+                if (req.body.data && oldData)
+                    project.data[oldDataIndex] = req.body.data;
+                if (req.body.data && !oldData)
                     project.data.push(req.body.data);
-                project.save(function(err) {
+                project.save(function (err) {
                     if (err)
                         return res.send(err);
 
@@ -272,16 +278,16 @@ module.exports = function(app) {
         });
     });
 
-    app.put('/architecture/:name', function(req, res) {
-        Architecture.findOne({ name: req.params.name }, function(err, project) {
+    app.put('/architecture/:name', function (req, res) {
+        Architecture.findOne({name: req.params.name}, function (err, project) {
             if (err) {
                 return res.send(err);
             }
-            if(!project) {
+            if (!project) {
                 let p = new Architecture();
                 p.name = req.params.name;
                 p.children = req.body.children;
-                p.save(function(err) {
+                p.save(function (err) {
                     if (err)
                         return res.send(err);
 
@@ -291,7 +297,7 @@ module.exports = function(app) {
                 project.name = req.body.name || project.name;
                 project.children = req.body.children || project.children;
 
-                project.save(function(err) {
+                project.save(function (err) {
                     if (err)
                         return res.send(err);
                     res.json(project);
